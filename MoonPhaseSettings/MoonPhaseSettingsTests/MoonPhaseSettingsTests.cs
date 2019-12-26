de4b08d0b16713eccb1c06922817d3477db05459
yeet648 <yeet648@apemojo.com>
2019-12-25 16:03:12 -0800

    add unit tests

---
+++ MoonPhaseSettings/MoonPhaseSettingsTests/MoonPhaseSettingsTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoonPhaseSettings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MoonPhaseSettings.Tests
+{
+    [TestClass()]
+    public class MoonPhaseSettingsTests
+    {
+        [TestMethod()]
+        public void Input_FileNotExist()
+        {
+            //check for input when fil does not exist
+            string[] args = {"foo"};
+            int expected = 20;
+            int result =MoonPhaseSettings.Main(args);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
