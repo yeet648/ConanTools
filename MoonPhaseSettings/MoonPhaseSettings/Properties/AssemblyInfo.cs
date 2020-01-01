8671d5617472310345448d9a35b5efc4b7ac8bbe
yeet648 <yeet648@apemojo.com>
2019-12-31 16:03:55 -0800

    added resource icon

--- MoonPhaseSettings/MoonPhaseSettings/Properties/AssemblyInfo.cs
+++ MoonPhaseSettings/MoonPhaseSettings/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MoonPhaseSettings")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Update Conan Exiles game settings based on real world moon phase.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Fist of Grayhawk")]
 [assembly: AssemblyProduct("MoonPhaseSettings")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -32,4 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
